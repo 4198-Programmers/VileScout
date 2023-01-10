c1617c410568ad94d1b6f9ea67e264cd09046ecb
Aiden Lee <aidenjlee9478@gmail.com>
2023-01-10 09:14:22 -0600

    Capitalized some metrics

--- index.js
+++ index.js
@@ -85,8 +85,8 @@ const infiniteRechargeSurvey = {
 
     { "name": "Programmed Auto Capabilities?", "type": "text", "tip": "Type here. Leave blank for none.", "group": "Programmed Capabilities" },
     { "name": "Limelight Capabilities:", "type": "select", "values": ["Auto Targeting & Auto Shooting", "Auto Targeting", "Auto Shooting", "None / No Limelight"] },
-    { "name": "april tags used?", "type":"toggle"},
-    { "name": "reflective tape used?", "type":"toggle"},
+    { "name": "April tags used?", "type":"toggle"},
+    { "name": "Reflective tape used?", "type":"toggle"},
     { "name": "Extra Cameras Used?", "type": "toggle" },
     { "name": "Automation Via Sensors?", "type": "toggle" },
 
